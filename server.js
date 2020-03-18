1bbcb1deb4dd12021e38b944e924c6d5cd95834a
Rapeepan Moonthai <rapeeza1598@gmail.com>
2020-03-19 02:01:13 +0700

    :pencil: create server
    create backend services and connect database

---
+++ server.js
@@ -0,0 +1,26 @@
+let express = require("express")
+let cors = require("cors")
+let bodyParser = require("body-parser")
+let app = express()
+let mongoose = require("mongoose")
+let port = process.env.PORT || 5000
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+)
+
+const mongoURI = 'mongodb://localhost:27017/mernloginreg'
+
+mongoose.connect(mongoURI, { useNewUrlParser: true }).then(() => console.log("MongoDB connected")).catch(err => console.log(err))
+
+let Users = require('./routes/Users')
+
+app.use('/users', Users)
+
+app.listen(port, () => {
+    console.log("Server is runing on port: " + port)
+})
